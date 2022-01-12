429db54a4073de6e9fadb14733e78f112c103644
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-01-12 17:26:50 -0600

    created prompts using inquirer, saved user input, connected functions

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your name?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your project name?",
+        name: "projName"
+    },
+    {
+        type: "input",
+        message: "Brief description of project:",
+        name: "projDesc"
+    },
+])
+    .then((answers) => {
+        console.log(answers);
+        var markupStr = writeToFile(answers);
+        console.log(markupStr);
+        fs.writeFile('newReadMe.md', markupStr,
+            (err) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log("Successful");
+            })
+    })
+    .catch(err => {
+        console.log(err);
+    })
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+function writeToFile(data) {
+    // console.log(data);
+    return generateMarkdown(data);
 
-// TODO: Create a function to initialize app
-function init() {}
+}
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,38 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return (`#${data.projName}
+  ## Description
+  ${data.projDesc}
+  ## Table of Contents (Optional)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  ## Usage
+          
+  ![alt text](assets/images/screenshot.png)
+      
+  ## Credits
+      
+  ## License
+   [https://choosealicense.com/](https://choosealicense.com/).
+  ---
+  ## How to Contribute
+  [Contributor Covenant](https://www.contributor-covenant.org/)
 
-`;
+`);
 }
 
 module.exports = generateMarkdown;
