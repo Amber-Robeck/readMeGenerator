9fbf93aa141065955b5d46186f23f88641370a86
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-01-13 22:35:51 -0600

    put prompt questions into array then put the inquire prompt into init funciton

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// TODO: Include packages needed for this application
+//packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer')
 const generateMarkdown = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
-// const questions = [];
-inquirer.prompt([
+//array of questions for user input
+const questions = [
+
     {
         type: "input",
         message: "What is your GitHub username?",
@@ -53,32 +53,36 @@ inquirer.prompt([
         choices: ["Skip", "ISC", "MIT", "Apache-2.0", "Gpl-3.0"]
     },
 
-])
-    .then((answers) => {
-        console.log(answers);
-        var markupStr = writeToFile(answers);
-        console.log(markupStr);
-        fs.writeFile('newReadMe.md', markupStr,
-            (err) => {
-                if (err) {
-                    console.log(err);
-                }
-                console.log("Successful");
-            })
-    })
-    .catch(err => {
-        console.log(err);
-    })
+];
 
-// // TODO: Create a function to write README file
+
+//function to write README file
 function writeToFile(data) {
-    // console.log(data);
     return generateMarkdown(data);
 
 }
 
-// // TODO: Create a function to initialize app
-// function init() {}
+//function to initialize app
+function init() {
+    inquirer.prompt(questions)
 
-// // Function call to initialize app
-// init();
+        .then((answers) => {
+            console.log(answers);
+            var markupStr = writeToFile(answers);
+            console.log(markupStr);
+            fs.writeFile('newReadMe.md', markupStr,
+                (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log("Successful");
+                })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+}
+
+// Function call to initialize app
+init();
