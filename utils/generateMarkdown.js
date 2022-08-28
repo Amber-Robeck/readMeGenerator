5898adfb5740249696450b4ecd4e842c45702e48
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-08-28 12:19:19 -0500

    Adding changes from original project

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,4 @@
-//[https://choosealicense.com/](https://choosealicense.com/).
-
-// https://img.shields.io/npm/l/c?style=for-the-badge MIT
-// https://choosealicense.com/licenses/mit/
-// https://img.shields.io/npm/l/s?style=for-the-badge Apache 2.0
-// https://choosealicense.com/licenses/apache-2.0/
-// https://img.shields.io/npm/l/v?style=for-the-badge Gpl 3.0
-// https://choosealicense.com/licenses/gpl-3.0/
-// https://img.shields.io/npm/l/d?style=for-the-badge ISC
-// https://choosealicense.com/licenses/isc/
-
-// TODO: Create a function that returns a license badge based on which license is passed in
+//function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(data) {
   if (data === "MIT") {
